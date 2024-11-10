c2ba1ce2c19ce2c55f6961f63d04083fe1071523
Prareenutt Rumpungsuk <prareenuttrumpungsuk@Prareenutts-Mac-mini.local>
2024-11-09 23:08:34 -0800

    Added keymaps and zenmode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,5 @@ local map = vim.keymap.set
 map("n", "<leader>kk", function()
   print("This editor will explode if you dont close it in 10s.")
 end, { desc = "Explode Message" })
+
+map("n", "<leader>z", ":ZenMode<CR>", { noremap = true, silent = true })
