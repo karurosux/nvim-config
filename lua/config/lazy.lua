1cdd259663ccff293f259743e4efa84c6b79b91e
Carlos Gonzalez <justdevelopitmx@gmail.com>
2024-11-25 10:12:59 -0800

    Added supermaven

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,28 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+      "supermaven-inc/supermaven-nvim",
+      config = function()
+        require("supermaven-nvim").setup({
+          keymaps = {
+            accept_suggestion = "<C-a>",
+            clear_suggestion = "<C-]>",
+            accept_word = "<C-j>",
+          },
+          ignore_filetypes = { cpp = true }, -- or { "cpp", }
+          color = {
+            cterm = 244,
+          },
+          log_level = "info", -- set to "off" to disable logging completely
+          disable_inline_completion = false, -- disables inline completion for use with cmp
+          disable_keymaps = false, -- disables built in keymaps for more manual control
+          condition = function()
+            return false
+          end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+        })
+      end,
+    },
     -- import/override with your plugins
     { import = "plugins" },
   },
